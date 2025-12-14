00124f11c766184f903eefac522ba8b24b4fe9f3
jiamei <jiamei@mit.edu>
2025-12-14 16:35:57 -0500

    leslie draft done

--- projects-data.js
+++ projects-data.js
@@ -1,13 +1,22 @@
 // Edit this file to add/update projects
 const projects = [
     {
+        title: "Leslie Speaker 'Pedal'",
+        date: "December 2025",
+        tags: ["power", "course"],
+        image: "./images/leslie_box.jpg", // Path to your image
+        description: "Designed a mechanical 'guitar pedal' based on a Leslie speaker with continuous speed control, featuring a Class D amplifier, boost converter, and SEPIC converter",
+        link: "projects/leslie-speaker.html"
+    },
+    {
         title: "Resonant Converter for Fluorescent Lamp Drive",
         date: "October 2025",
         tags: ["power", "course"],
         image: "./images/lamp_wave1.jpg", // Path to your image
         description: "Designed and built an LC resonant converter to drive a fluorescent lamp, achieving 400V strike voltage from a 30V input",
-        link: "projects/resonant-converter.html" // Link to project details or leave as "#"
+        link: "projects/resonant-converter.html"
     },
+
 ];
 
 // Available filter categories - add more as needed
